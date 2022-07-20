a0b5e35ff2326bb5a6b7c1a243e52266b008320e
KyeongeunSon9 <103475258+KyeongeunSon9@users.noreply.github.com>
2022-07-20 12:59:59 +0900

    07-20

---
+++ 07-20/dom요소 가져오기.js
@@ -0,0 +1,14 @@
+const queryClass = doncument.querySelector('.class')
+const queryId = document.querySelector('#id')
+// 쿼리 셀렉터로 가져오기
+
+
+const getId = document.getElementById('id')
+// id로 가져오기
+
+
+const getId = document.getElementsByClassName('class')
+// class로 가져오기
+
+/* class로 가져올 경우 여러개의 돔요소를 가져오기때문에
+getClass[0]과 같은 형태로 사용 가능하다 */
